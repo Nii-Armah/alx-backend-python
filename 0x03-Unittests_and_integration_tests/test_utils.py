3868cda7cf970a61b5df3c2e77e857f8676bbf60
Nii-Armah <stanleyniiarmah7@gmail.com>
2025-07-15 21:04:46 +0000

    Created initial test case for utils.access_nested_map

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,17 @@
+"""Tests for utilities module."""
+
+import unittest
+
+from utils import access_nested_map
+
+from parameterized import parameterized
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    @parameterized.expand([
+        ({'a': 1}, ('a',), 1),
+        ({'a': {'b': 2}}, ('a',), {'b': 2}),
+        ({'a': {'b': 2}}, ('a', 'b'), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected_output) -> None:
+        self.assertEqual(access_nested_map(nested_map, path), expected_output)
