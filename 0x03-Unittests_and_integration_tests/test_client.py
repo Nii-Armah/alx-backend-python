b4dfb9426f909000da2ba5bc491180d1665983e0
Nii-Armah <stanleyniiarmah7@gmail.com>
2025-07-16 10:26:55 +0000

    Fixed typo in test method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -88,7 +88,7 @@ class TestGithubOrgClient(unittest.TestCase):
         ({'license': {'key': 'my_license'}}, 'my_license', True),
         ({'license': {'key': 'other_license'}}, 'my_license', False)
     ])
-    def test_has_licence(self, license_obj, license_key, result) -> None:
+    def test_has_license(self, license_obj, license_key, result) -> None:
         """
         Validate return value of has_licence attribute of a GithubOrgClient
         instance.
