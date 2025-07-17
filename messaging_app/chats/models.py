28e345e85caf14128d9124f6a4e2f06e9347420b
Nii-Armah <stanleyniiarmah7@gmail.com>
2025-07-17 07:42:50 +0000

    Created a custom User model

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,24 @@
+"""
+Data models for chats application.
+
+User:
+    A user of the system.
+"""
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+import uuid
+
+
+class User(AbstractUser):
+    """A user of the system."""
+    class Meta:
+        db_table = 'users'
+
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        help_text='Unique identification of user'
+    )
