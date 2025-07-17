ec07d4e76498686d4fcf8c396cccf25fa87f8fed
Nii-Armah <stanleyniiarmah7@gmail.com>
2025-07-17 08:41:43 +0000

    Created viewsets for Conversation and Message models

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+"""
+API endpoints for chats application.
 
-# Create your views here.
+ConversationViewSet:
+    Handles management of conversations.
+
+MessageViewSet:
+    Handles management of messages.
+"""
+
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
+
+from rest_framework import viewsets
+
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    """Handles management of conversations."""
+    queryset = Conversation.objects.select_related('participants', 'messages').all()
+    serializer_class = ConversationSerializer
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    """Handles management of messages."""
+    queryset = Message.objects.select_related('conversation').all()
+    serializer_class = MessageSerializer
