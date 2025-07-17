a3921b9ced33d3c855f1620f9314345a1b171b1f
Nii-Armah <stanleyniiarmah7@gmail.com>
2025-07-17 09:29:32 +0000

    Updated urls.py

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -4,10 +4,10 @@ URL configuration for chats application.
 
 from chats.views import MessageViewSet, ConversationViewSet
 
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 from rest_framework_nested.routers import NestedDefaultRouter
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversations')
 
 nested_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
