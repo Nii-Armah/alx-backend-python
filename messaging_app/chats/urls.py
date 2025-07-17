8d084f2017770f4987891317e467239fb1918d36
Nii-Armah <stanleyniiarmah7@gmail.com>
2025-07-17 08:48:02 +0000

    Configured internal urls for chats application

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -2,4 +2,17 @@
 URL configuration for chats application.
 """
 
-urlpatterns = []
+from django.urls import path
+
+from messaging_app.chats.views import MessageViewSet, ConversationViewSet
+
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+router.register(r'messages', MessageViewSet)
+router.register(r'conversations', ConversationViewSet)
+
+urlpatterns = [
+    path('', router.urls),
+]
