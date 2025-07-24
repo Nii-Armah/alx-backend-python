ab3fb340a2a0da1299f67955a195fa0b8eae7e3f
Nii-Armah <stanleyniiarmah7@gmail.com>
2025-07-24 08:15:56 +0000

    Updated IsParticipantOfConversation
    Enforces access by only authenticated users.

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -10,5 +10,8 @@ from rest_framework import permissions
 
 class IsParticipantOfConversation(permissions.BasePermission):
     """User is a participant of Conversation."""
+    def has_permission(self, request, view):
+        return request.user.is_authenticated
+
     def has_object_permission(self, request, view, obj):
         return obj.participants.filter(user_id=request.user.id).exists()
