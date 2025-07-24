6313e7a022baf7d4eb802f1c2524209ad0f48c54
Nii-Armah <stanleyniiarmah7@gmail.com>
2025-07-24 08:19:24 +0000

    Updated messagin_app.views.py

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -12,14 +12,14 @@ from .models import Conversation, Message
 from .permissions import  IsParticipantOfConversation
 from .serializers import ConversationSerializer, MessageSerializer
 
-from rest_framework import viewsets, status, filters
+from rest_framework import viewsets, status, filters, permissions
 
 
 class ConversationViewSet(viewsets.ModelViewSet):
     """Handles management of conversations."""
     queryset = Conversation.objects.select_related('participants', 'messages').all()
     serializer_class = ConversationSerializer
-    permission_classes = [IsParticipantOfConversation, ]
+    permission_classes = [permissions.IsAuthenticated, IsParticipantOfConversation, ]
 
 
 class MessageViewSet(viewsets.ModelViewSet):
