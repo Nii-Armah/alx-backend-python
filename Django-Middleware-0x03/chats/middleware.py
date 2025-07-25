77b24b532cfab1cfba81dc6e064943ea5dfe18a9
Nii-Armah <stanleyniiarmah7@gmail.com>
2025-07-25 08:21:22 +0000

    Created RestrictAccessByTimeMiddleware
    Restricts access to the application between 6pm and 9pm (inclusive) each day.

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -3,7 +3,15 @@ Middlewares specific to chats application.
 
 RequestLoggingMiddleware:
     Logs each request including the timestamp, user and request path.
+
+RestrictAccessByTimeMiddleware:
+    Restricts access to the application between 6pm and 9pm (inclusive) each day.
 """
+
+from django.http import HttpResponse
+from django.utils import timezone
+
+from rest_framework import status
 
 import datetime
 
@@ -16,5 +24,17 @@ class RequestLoggingMiddleware:
     def __call__(self, request):
         with open('requests.log', 'a') as log_file:
             log_file.write(f'{datetime.datetime.now()} - User: {request.user} - Path: {request.path}\n')
+
+        return self.get_response(request)
+
+
+class RestrictAccessByTimeMiddleware:
+    """Restricts access to the application between 6pm and 9pm (inclusive) each day."""
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if 18 <= timezone.now().hour <= 21:
+            return HttpResponse(status=status.HTTP_403_FORBIDDEN)
 
         return self.get_response(request)
--- Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
 
     # Custom Middleware
     'chats.middleware.RequestLoggingMiddleware',
+    'chats.middleware.RestrictAccessByTimeMiddleware',
 ]
 
 ROOT_URLCONF = 'messaging_app.urls'
